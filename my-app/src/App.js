9f3e1b3229cda224a3e4d3090a7b78e9046fbc97
aocho5 <anahi523ochoa@gmail.com>
2025-05-15 13:40:50 -0500

    profile picture

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import screenshot from './images/screenshot-2025-04-15.png';
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <img src={screenshot} alt="Screenshot" className="Profile-image" />
         <a
           className="App-name"
           href="https://reactjs.org"
