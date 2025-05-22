86263a1806628e785cb26b21b4ab92ee441d74e1
aocho5 <anahi523ochoa@gmail.com>
2025-05-22 17:44:47 -0500

    new background and menu box

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 import screenshot from './images/screenshot-2025-04-15.png';
-
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img src={screenshot} alt="Screenshot" className="Profile-image" />
-        <a
-          className="App-name"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Anahi Ochoa
-        </a>
+        <div className="Profile-container">
+          <div className="Profile-info">
+            <img src={screenshot} alt="Screenshot" className="Profile-image" />
+            <a
+              className="App-name"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+             Anahi Ochoa
+            </a>
+          </div>
+          <div className="Rectangle" />
+        </div>
       </header>
     </div>
   );
